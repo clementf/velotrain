a929e819681101101c93a01ea56edda57155a980
Clement Ferey <clement.ferey@gmail.com>
2024-07-02 15:08:26 +0200

    Take into account transfers within Paris
    Those can generally be considered possible, especially given how many
    more options they allow for.

--- app/models/routing/time_dependent_graph.rb
+++ app/models/routing/time_dependent_graph.rb
@@ -11,11 +11,31 @@ module Routing
     end
 
     def neighbors(node, current_time, current_route_id)
+      reachable_stations(node, current_time, current_route_id) + transfers_within_paris(node, current_time)
+    end
+
+    private
+
+    def reachable_stations(node, current_time, current_route_id)
       @graph[node].select do |edge|
         transfer_time_if_needed = (current_route_id.present? && current_route_id != edge[:route_id]) ? 15 * 60 : 0
 
         edge[:departure_time] >= current_time + transfer_time_if_needed
       end
+    end
+
+    def transfers_within_paris(node, current_time)
+      return [] unless stations_in_paris.include?(node)
+
+      paris_stations = @graph.keys.select { |station| stations_in_paris.include?(station) }
+
+      paris_stations.map do |paris_station|
+        {to: paris_station, departure_time: current_time, arrival_time: current_time + 60 * 60, route_id: nil}
+      end
+    end
+
+    def stations_in_paris
+      @stations_in_paris ||= Gtfs::Stop.train_stations.where("name LIKE ?", "%Paris%").pluck(:id)
     end
   end
 end
