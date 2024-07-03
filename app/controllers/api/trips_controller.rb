cd5fe436c751a9bfefde551c71f99ff7485a45c4
Clement Ferey <clement.ferey@gmail.com>
2024-07-03 09:07:04 +0200

    Update url when changing trip, so they can be shared

--- app/controllers/api/trips_controller.rb
+++ app/controllers/api/trips_controller.rb
@@ -1,39 +1,5 @@
 module Api
   class TripsController < ApplicationController
-    def index
-      router = Routing::Router.get
-
-      from = Gtfs::Stop.find_by(id: params[:from_stop_id])
-      to = Gtfs::Stop.find_by(id: params[:to_stop_id])
-
-      if from.nil? || to.nil?
-        @results = []
-        render "trips/index", status: :bad_request
-        return
-      end
-
-      parsed_hour_from_params = begin
-        ActiveSupport::TimeZone["Europe/Paris"].parse(params["hour"] + ":" + params["minute"])
-      rescue
-        nil
-      end
-
-      # generate shortest path for now, and the next 4 possible trains
-      @results = []
-
-      4.times do
-        start_time = (@results.last&.dig(:path, 0, :departure_time)&.+ 1.minutes) || parsed_hour_from_params || Time.current
-
-        result = router.shortest_path(from, to, start_time.utc.strftime("%H:%M:%S"))
-        break if result[:path].empty?
-
-        @results << result
-      end
-
-      @stop_ids = @results.first[:path].flatten.map { |segment| segment[:to] }.prepend(@results.first[:start]).join(",") if @results.any?
-      render "trips/index"
-    end
-
     def show
       stop_ids = params[:stop_ids].split(",").map(&:to_i)
       path = []
