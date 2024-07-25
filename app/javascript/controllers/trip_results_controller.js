13cfdb931fc78865228b3fcab92ff7692a87da75
Clement Ferey <clement.ferey@gmail.com>
2024-07-25 19:09:34 +0200

    Highlight trip on mouse over rather than scroll

--- app/javascript/controllers/trip_results_controller.js
+++ app/javascript/controllers/trip_results_controller.js
@@ -22,7 +22,6 @@ export default class extends Controller {
 
     // Still need to check if the map is loaded when the load event is fired
     window.map.on('load', () => {
-      console.log('Map loaded');
       this.setupObserver();
     });
   }
@@ -32,64 +31,59 @@ export default class extends Controller {
   }
 
   setupObserver() {
-    let observer = new IntersectionObserver(this.callback.bind(this), {
-      root: null, // Use the viewport as the container
-      rootMargin: '0px', // No margin around the root
-      threshold: 0.1 // Trigger when at least 10% of the element is visible
-    });
-
     let targets = document.querySelectorAll('.trip');
+
+    this.showSection(targets[0]);
+
     targets.forEach(target => {
-      observer.observe(target);
+      target.addEventListener('mouseenter', (event) => {
+        this.showSection(target);
+      })
     });
   }
 
-  callback(entries, observer) {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        let idx = 0;
+  showSection(target) {
+    let idx = 0;
 
-        this.removeLayers();
-        window.section_layers = [];
+    this.removeLayers();
+    window.section_layers = [];
 
-        let bounds = new maplibregl.LngLatBounds();
+    let bounds = new maplibregl.LngLatBounds();
 
-        entry.target.querySelectorAll('.section').forEach((section) => {
-          JSON.parse(section.dataset.geoJson).coordinates.forEach((coord) => {
-            bounds.extend(coord);
-          });
-        });
+    target.querySelectorAll('.section').forEach((section) => {
+      JSON.parse(section.dataset.geoJson).coordinates.forEach((coord) => {
+        bounds.extend(coord);
+      });
+    });
 
-        window.map.fitBounds(bounds, {
-          duration: 400,
-          padding: { top: 50, bottom: 50, left: document.querySelector('#search-container').offsetWidth + 50, right: 50 }
-        });
+    window.map.fitBounds(bounds, {
+      duration: 200,
+      padding: { top: 50, bottom: 50, left: document.querySelector('#search-container').offsetWidth + 50, right: 50 }
+    });
 
-        entry.target.querySelectorAll('.section').forEach((section) => {
-          idx += 1;
-          window.section_layers.push("section_" + idx);
+    target.querySelectorAll('.section').forEach((section) => {
+      idx += 1;
+      window.section_layers.push("section_" + idx);
 
-          window.map.addSource("section_" + idx, {
-            type: "geojson",
-            data: JSON.parse(section.dataset.geoJson),
-          });
+      window.map.addSource("section_" + idx, {
+        type: "geojson",
+        data: JSON.parse(section.dataset.geoJson),
+      });
 
-          window.map.addLayer({
-            id: "section_" + idx,
-            type: "line",
-            source: "section_" + idx,
-            layout: {
-              "line-join": "round",
-              "line-cap": "round",
-            },
-            paint: {
-              "line-color": "#065f46",
-              "line-opacity": 0.8,
-              "line-width": 5,
-            },
-          });
-        });
-      }
+      window.map.addLayer({
+        id: "section_" + idx,
+        type: "line",
+        source: "section_" + idx,
+        layout: {
+          "line-join": "round",
+          "line-cap": "round",
+        },
+        paint: {
+          "line-color": "#065f46",
+          "line-opacity": 0.8,
+          "line-width": 5,
+        },
+      });
     });
   }
 
