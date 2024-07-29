58b76a2e669de3fb7dd44d39e7d823bd23071b16
Clement Ferey <clement.ferey@gmail.com>
2024-07-29 10:10:03 +0200

    Get stats on visits

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "ahoy"
+
+ahoy.configure({cookies: false, trackVisits: true})
+
+document.addEventListener("turbo:load", function() {
+  ahoy.trackView();
+})
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "maplibre-gl" # @4.3.2
+pin "ahoy" # @1.0.1
+pin "os" # @2.0.1
