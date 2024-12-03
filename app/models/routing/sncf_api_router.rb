a2e54b79d9dee49c766bb6ccda62163d5fda4dcd
Clement Ferey <clement.ferey@gmail.com>
2024-12-03 19:07:16 +0100

    Fix list of allowed networks
    TER is not being used anymore, all regional versions are used instead.

--- app/models/routing/sncf_api_router.rb
+++ app/models/routing/sncf_api_router.rb
@@ -151,9 +151,31 @@ module Routing
       "https://api.sncf.com/v1/coverage/sncf/journeys?from=#{from}&to=#{to}&datetime=#{datetime}&#{allowed_ids}&min_nb_journeys=#{min_nb_journeys}&max_nb_journeys=#{max_nb_journeys}&forbidden_uris[]=physical_mode:Coach"
     end
 
-    # list of networks: https://api.sncf.com/v1/coverage/sncf/networks
+    # list of networks: https://api.sncf.com/v1/coverage/sncf/networks (username is api key, pwd is empty)
     def allowed_ids
-      "allowed_id[]=network:SNCF:IC&allowed_id[]=network:SNCF:TER&allowed_id[]=network:SNCF:ICN&allowed_id[]=network:SNCF:OUIGO_TC&allowed_id[]=network:SNCF:TERGV&allowed_id[]=network:SNCF:FR:Branding::6dfb1ec4-50b5-42a8-a531-a792b1ea6f2e:&allowed_id[]=network:SNCF:FR:Branding::5c5f14b9-bff2-4f94-8853-0f621bf1a1cf:&allowed_id[]=physical_mode:Bike"
+      allowed_networks = {
+        ic: "network:SNCF:IC",
+        ter: "network:SNCF:TER",
+        icn: "network:SNCF:ICN", 
+        ouigo: "network:SNCF:OUIGO_TC",
+        tergv: "network:SNCF:TERGV",
+        aleop: "network:SNCF:FR:Branding::e6a26019-486f-48d6-bb07-c24cae7fc307:",
+        breizhgo: "network:SNCF:FR:Branding::7500b3d5-ea82-4597-ad48-edd896ca9ed0:",
+        fluo: "network:SNCF:FR:Branding::c33db331-f39b-4e39-965d-053331becc07:",
+        lex: "network:SNCF:FR:Branding::a80c322c-12b1-4273-9413-e2df5dd6dce8:",
+        mobigo: "network:SNCF:FR:Branding::fa03c60c-432a-4c27-b646-e231f19253c6:",
+        nomad: "network:SNCF:FR:Branding::3b6b9052-6f3e-4ea4-866f-3bd913db6b22:",
+        regionaura: "network:SNCF:FR:Branding::f4fa116c-2d6a-4696-b6c9-47195206d6f4:",
+        remi: "network:SNCF:FR:Branding::d5d601cf-e383-4138-8e70-ee1b7fcadd48:",
+        remi_exp: "network:SNCF:FR:Branding::8c1ce319-d566-4575-a416-a46730280ce5:",
+        solea: "network:SNCF:FR:Branding::45d07532-11f7-4837-98b7-cae82b0ab0c4:",
+        transilien: "network:SNCF:TN",
+        bike: "physical_mode:Bike"
+      }
+
+      allowed_networks.values
+        .map { |id| "allowed_id[]=#{id}" }
+        .join("&")
     end
   end
 end
