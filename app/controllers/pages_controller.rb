a122dbd5348f5e6b087993c4b67a484811964ba2
Clement Ferey <clement.ferey@gmail.com>
2025-03-02 10:13:54 +0100

    Support entering a custom date for a trip (defaults to today)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,18 +13,28 @@ class PagesController < ApplicationController
       return
     end
 
+    # Parse the date from params or use current date
+    selected_date = params[:date].present? ? Date.parse(params[:date]) : Date.current
+
+    # Make sure date is not in the past
+    selected_date = Date.current if selected_date < Date.current
+
+    # Make sure date is not more than 2 months in the future
+    selected_date = Date.current + 2.months if selected_date > Date.current + 2.months
+
     parsed_hour_from_params = begin
-      ActiveSupport::TimeZone["Europe/Paris"].parse(params["hour"] + ":00")
+      hour_value = params["hour"].presence || "08"
+      ActiveSupport::TimeZone["Europe/Paris"].parse("#{selected_date.to_s} #{hour_value}:00")
     rescue
-      nil
+      ActiveSupport::TimeZone["Europe/Paris"].parse("#{selected_date.to_s} 08:00")
     end
 
-    # default to 8:00 if no hour is provided
-    if parsed_hour_from_params.nil?
-      parsed_hour_from_params = ActiveSupport::TimeZone["Europe/Paris"].parse("08:00")
-    end
+    saved_search = SavedSearch.find_or_initialize_by(
+      from_stop: @from.gtfs_stop,
+      to_stop: @to.gtfs_stop,
+      datetime: parsed_hour_from_params
+    )
 
-    saved_search = SavedSearch.find_or_initialize_by(from_stop: @from.gtfs_stop, to_stop: @to.gtfs_stop, datetime: parsed_hour_from_params)
     if saved_search.persisted?
       @results = saved_search.results.map { |result| result.with_indifferent_access }
     else
